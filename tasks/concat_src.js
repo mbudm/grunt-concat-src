55820f14ae9b5520bc11b3fc45d6374e3d271323
Steve Roberts <steve.roberts@xero.com>
2015-06-16 22:22:27 +1000

    handle absolute paths

--- tasks/concat_src.js
+++ tasks/concat_src.js
@@ -12,61 +12,65 @@ var path = require('path');
 
 module.exports = function(grunt) {
 
-  grunt.registerMultiTask('concat_src', 'Parse src attributes and concat into a bundle.js', function(arg1) {
+	grunt.registerMultiTask('concat_src', 'Parse src attributes and concat into a bundle.js', function(arg1) {
 
-    var options = this.options({
-      separator: '',
-      dest:'dist',
-      reflectPath:false
-    });
-
-
-    var legitFiles = this.filesSrc.filter(function(f) {
-      // Warn on and remove invalid source files.
-      if (!grunt.file.exists(f)) {
-        grunt.log.warn('Source file "' + f + '" not found.');
-        return false;
-      }else{
-        return true;
-      }
-    });
-
-    // Parse files for assets
-    legitFiles.forEach(function(f){
-
-      var fileSrc = grunt.file.read(f);
-      var lines = fileSrc.replace(/\r\n/g, '\n').split(/\n/);
-      var srcDir = path.dirname(f);
-      var bundleSrc = lines.map(function(line) {
-        var asset = (line.match(/(src)=["']([^'"]+)["']/) || [])[2];
-        if(!asset){
-          return false; //fail early
-        }
-
-        // Warn on and remove invalid asset files
-        if (!grunt.file.exists(srcDir, asset)) {
-          grunt.log.warn('Asset "' +  asset + '" listed in "' + srcDir + '" not found.');
-          return false;
-        }else{
-          return path.join(srcDir, asset);
-        }
-      }).filter(function(assetPath){
-        return assetPath ? true : false;
-      }).map(function(assetPath){
-        return grunt.file.read(assetPath);
-      }).join(grunt.util.normalizelf(options.separator));
+		var options = this.options({
+			separator: '',
+			dest: 'dist',
+			reflectPath: false
+		});
 
 
-      var bundlePath = options.reflectPath ? f : path.basename(f);
-      var dest = path.join(options.dest, bundlePath);
+		var legitFiles = this.filesSrc.filter(function(f) {
+			// Warn on and remove invalid source files.
+			if (!grunt.file.exists(f)) {
+				grunt.log.warn('Source file "' + f + '" not found.');
+				return false;
+			} else {
+				return true;
+			}
+		});
 
-      // Write the bundle file.
-      grunt.file.write(dest, bundleSrc);
+		// Parse files for assets
+		legitFiles.forEach(function(f) {
 
-      // Print a success message.
-      grunt.log.writeln('Bundle "' + dest + '" created.');
-    });
+			var fileSrc = grunt.file.read(f);
+			var lines = fileSrc.replace(/\r\n/g, '\n').split(/\n/);
+			var srcDir = path.dirname(f);
+			var bundleSrc = lines.map(function(line) {
+				var asset = (line.match(/(src)=["']([^'"]+)["']/) || [])[2];
+				if (!asset) {
+					return false; //fail early
+				}
 
-  });
+				//handle absolute paths
+				var baseDir = asset.charAt(0) === '/' ? '' : srcDir;
+				var assetCorrected = asset.charAt(0) === '/' ? '.' + asset : asset;
+
+				// Warn on and remove invalid asset files
+				if (!grunt.file.exists(baseDir, assetCorrected)) {
+					grunt.log.warn('Asset "' + path.join(baseDir, assetCorrected) + '" listed in "' + f + '" not found.');
+					return false;
+				} else {
+					return path.join(baseDir, assetCorrected);
+				}
+			}).filter(function(assetPath) {
+				return assetPath ? true : false;
+			}).map(function(assetPath) {
+				return grunt.file.read(assetPath);
+			}).join(grunt.util.normalizelf(options.separator));
+
+
+			var bundlePath = options.reflectPath ? f : path.basename(f);
+			var dest = path.join(options.dest, bundlePath);
+
+			// Write the bundle file.
+			grunt.file.write(dest, bundleSrc);
+
+			// Print a success message.
+			grunt.log.writeln('Bundle "' + dest + '" created.');
+		});
+
+	});
 
 };
--- test/fixtures/two.js
+++ test/fixtures/two.js
@@ -1,5 +1,5 @@
-var two = function two(p){
-  return p;
+var two = function two(p) {
+	return p;
 };
 
 console.log(two(one));
